351e9b6bd59dfb8c2fa3c4fb2ddfa1ffa46b168d
pvega7721 <pvega7721@ceuandalucia.es>
2024-10-02 20:47:51 +0200

    .

--- Java/Ejercicio9/src/main/java/com/example/demo/controllers/AlumnoController.java
+++ Java/Ejercicio9/src/main/java/com/example/demo/controllers/AlumnoController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controllers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +31,62 @@ public class AlumnoController {
 		alumnos.add(alumno3);
 		alumnos.add(alumno4);
 	}
+
 	@GetMapping
 	public List<Alumno> mostrarAlumnos() {
 		return alumnos;
 	}
+
+	@GetMapping("/{email}")
+	public Alumno mostrarAluno(@PathVariable String email) {
+		for (Alumno alumno : alumnos) {
+			if (alumno.getEmail().equals(email)) {// Para comparar cadenas se usa .equals()
+				return alumno;
+			}
+		}
+		return null;
+
+	}
+
+	@PostMapping
+	public Alumno postAlumno(@RequestBody Alumno alumno) {
+		alumnos.add(alumno);
+		return alumno;
+	}
+
+	@PatchMapping
+	public Alumno modAlumno(@RequestBody Alumno mod) {
+		for (Alumno alumno : alumnos) {
+			if (alumno.getId() == mod.getId()) {
+				if (mod.getNombre() != null) {
+					alumno.setNombre(mod.getNombre());
+				}
+				if (mod.getCurso() != null) {
+					alumno.setCurso(mod.getCurso());
+				}
+				if (mod.getEdad() != null) {
+					alumno.setEdad(mod.getEdad());
+				}
+				if (mod.getEmail() != null) {
+					alumno.setEmail(mod.getEmail());
+				}
+				return alumno;
+			}
+		}
+		return null;
+	}
+
+	@DeleteMapping("{id}")
+	public Alumno deleteAumno(@PathVariable Integer id) {
+		Iterator<Alumno> iterador = alumnos.iterator();
+		while (iterador.hasNext()) {
+			Alumno alumno = iterador.next();
+			if (alumno.getId() == id) {
+				iterador.remove();
+				return alumno;
+			}
+		}
+		return null;
+	}
+
 }
