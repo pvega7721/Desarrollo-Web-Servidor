0bf26cf1c2c233bc1c4d4b95299ebc78809833d3
Pablo Vega <pvega7721@ceuandalucia.es>
2024-10-16 11:16:51 +0200

    .

--- Java/Ejercicio12/src/main/java/com/example/demo/controller/LibroController.java
+++ Java/Ejercicio12/src/main/java/com/example/demo/controller/LibroController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,12 +28,12 @@ public class LibroController {
 
 	public LibroController() {
 		genero1.add("Drama");
-		genero1.add("Triller");
+		genero1.add("Novela");
 		genero2.add("Comedia");
 		genero2.add("Fantasía");
 		genero3.add("Economía");
 		genero3.add("Cuentos infantiles");
-		genero4.add("Drama");
+		genero4.add("Novela");
 		genero4.add("Fantasía");
 
 		Libro libro1 = new Libro(1, "Titulo1", "Autor1", "editorial1", "isbn1", 2015, genero1);
@@ -45,12 +47,13 @@ public class LibroController {
 		libros.add(libro4);
 	}
 
+	// Mostrar todos los libros
 	@GetMapping
 	public ResponseEntity<List<Libro>> mostrarLibros() {
 		return ResponseEntity.ok(libros);
 	}
-	// Mostrar todos los libros
 
+	// Consultar un libro por su título
 	@GetMapping("/{titulo}")
 	public ResponseEntity<Libro> mostrarLibroTitulo(@PathVariable String titulo) {
 		for (Libro libro : libros) {
@@ -61,14 +64,16 @@ public class LibroController {
 		return ResponseEntity.notFound().build();
 	}
 
+	// Crear un libro nuevo
 	@PostMapping
 	public ResponseEntity<Void> anadirLibro(@RequestBody Libro libro) {
 		libros.add(libro);
 		return ResponseEntity.noContent().build();
 	}
 
+	// Modificar un libro de manera total
 	@PutMapping
-	public ResponseEntity<Void> modLibro(@RequestBody Libro mod) {
+	public ResponseEntity<Void> modLibroTotal(@RequestBody Libro mod) {
 		for (Libro libro : libros) {
 			if (libro.getId() == mod.getId()) {
 				libro.setAnoPublicacion(mod.getAnoPublicacion());
@@ -82,4 +87,86 @@ public class LibroController {
 		return ResponseEntity.noContent().build();
 	}
 
+	// Modificar un libro de manera parcial
+	@PatchMapping
+	public ResponseEntity<Void> modLibroParcial(@RequestBody Libro mod) {
+		for (Libro libro : libros) {
+			if (libro.getId() == mod.getId()) {
+				if (libro.getAnoPublicacion() != null) {
+					libro.setAnoPublicacion(mod.getAnoPublicacion());
+				}
+				if (libro.getAutor() != null) {
+					libro.setAutor(mod.getAutor());
+				}
+				if (libro.getEditorial() != null) {
+					libro.setEditorial(mod.getEditorial());
+				}
+				if (libro.getGenero() != null) {
+					libro.setGenero(mod.getGenero());
+				}
+				if (libro.getIsbn() != null) {
+					libro.setIsbn(mod.getIsbn());
+				}
+				if (libro.getTitulo() != null) {
+					libro.setTitulo(mod.getTitulo());
+				}
+				return ResponseEntity.noContent().build();
+			}
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	// Eliminar un libro por su ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> borrarLibro(@PathVariable Integer id) {
+		for (Libro libro : libros) {
+			if (libro.getId() == id) {
+				libros.remove(libro);
+				return ResponseEntity.noContent().build();
+			}
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	// Obtener libros cuyo género sea novela
+	@GetMapping("/novelas")
+	public ResponseEntity<List<Libro>> obtenerNovelas() {
+		List<Libro> novelas = new ArrayList<>();
+		for (Libro libro : libros) {
+			if (libro.getGenero().contains("Novela")) {
+				novelas.add(libro);
+			}
+		}
+		if (novelas.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.ok(novelas);
+		}
+	}
+	
+	//Obtener libros por género
+	@GetMapping("/{genero}")
+	public ResponseEntity<List<Libro>> obtenerLibroGenero(@PathVariable String genero){
+		List<Libro> porGenero = new ArrayList<>();
+		for(Libro libro: libros) {
+			if(libro.getGenero().contains(genero)) {
+				porGenero.add(libro);
+			}
+		}
+		if(porGenero.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}else {
+			return ResponseEntity.ok(porGenero);
+		}
+		
+	}
+	//NO FUNCIONA
+	/*
+	@GetMapping("/{numLibros}")
+	public ResponseEntity<List<Libro>> obtenerAutoresConMasDeXLibros(@PathVariable Integer numLibros){
+		
+	}
+	*/
+	
+	
 }
