399b50ce58261bd1b465456f01ef44405ad3dfac
Pablo Vega <pvega7721@ceuandalucia.es>
2024-10-23 09:30:02 +0200

    .

--- PHP/cerveceria/mostrarDatos.php
+++ PHP/cerveceria/mostrarDatos.php
@@ -8,12 +8,12 @@
 <body>
     <p>Tipo de cerveza: <?php echo $_REQUEST["tipoCerveza"] ?> </p>
     <p>Denominación del alimento: <?php echo $_REQUEST["Denominación"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["embase"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["CantidadNeta"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["marcaProducto"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["abusoConsumo"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["fechaConsumo"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["alergeno"] ?> </p>
-    <p>Denominación del alimento: <?php echo $_REQUEST["Observaciones"] ?> </p>
+    <p>Tipo de envase: <?php echo $_REQUEST["embase"] ?> </p>
+    <p>Cantidad neta: <?php echo $_REQUEST["CantidadNeta"] ?> </p>
+    <p>Marca del producto: <?php echo $_REQUEST["marcaProducto"] ?> </p>
+    <p>Advertencia sobre el abuso en el consumo de alcohol: <?php echo $_REQUEST["abusoConsumo"] ?> </p>
+    <p>Fecha de consumo preferente: <?php echo $_REQUEST["fechaConsumo"] ?> </p>
+    <p>Sustancias que pueden causar alergias: <?php echo $_REQUEST["alergeno"] ?> </p>
+    <p>Observaciones: <?php echo $_REQUEST["Observaciones"] ?> </p>
 </body>
 </html>
