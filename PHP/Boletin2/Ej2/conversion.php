7e8cc3a6c797397cad330461318aeab322996a51
pvega7721 <pvega7721@ceuandalucia.es>
2024-11-06 20:09:45 +0100

    .

---
+++ PHP/Boletin2/Ej2/conversion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversión de variables</title>
+</head>
+<body>
+    <?php 
+    $null = null;
+    $booleano1 = true;
+    $booleano2 = false;
+    $entero = 0;
+    $double = 3.8;
+    $cadena = "0";
+    $cadena2 = "10";
+    $distancia  = "6 metros";
+    $saludo = "hola";
+    ?>
+</body>
+</html>
