8715af4d77bcc21dc456603917eb81ec2cfb506b
pvega7721 <pvega7721@ceuandalucia.es>
2025-01-17 16:49:50 +0100

    Agregar estructura inicial del proyecto T4-Ej12 con configuración de Maven y pruebas básicas

---
+++ Java/T4-Ej12/src/main/java/com/example/demo/T4Ej12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T4Ej12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T4Ej12Application.class, args);
+	}
+
+}
