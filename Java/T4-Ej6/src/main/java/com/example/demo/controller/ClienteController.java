c6d88a2813bbfc541e167724228823d558fcd961
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-21 16:02:08 +0100

    Actualizar configuración de base de datos y mejorar el controlador de clientes

--- Java/T4-Ej6/src/main/java/com/example/demo/controller/ClienteController.java
+++ Java/T4-Ej6/src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
-public class ClienteController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	@Autowired
+	private ClienteService servicie;
+	
+	
+	
 }
