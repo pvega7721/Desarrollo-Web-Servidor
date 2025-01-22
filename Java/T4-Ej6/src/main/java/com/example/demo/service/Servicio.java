222c70b7ac8766f554fec1f2c0a6036815ab15ac
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-22 12:56:10 +0100

    .

--- Java/T4-Ej6/src/main/java/com/example/demo/service/Servicio.java
+++ Java/T4-Ej6/src/main/java/com/example/demo/service/Servicio.java
@@ -1,5 +1,27 @@
 package com.example.demo.service;
 
-public class ClienteService {
+import java.util.List;
 
+import com.example.demo.modelo.Cliente;
+import com.example.demo.modelo.Direccion;
+
+public interface Servicio { // Debe llevar interface
+
+	public List<Cliente> getClientes();
+
+	public Cliente getCliente(Integer id);
+
+	public void insertarCliente(Cliente c);
+
+	public Cliente actualizarCliente(Cliente c);
+
+	public boolean eliminarCliente(Integer id);
+
+	public Cliente actualizarDireccion(Integer id, Direccion d);
+
+	public void actualizarSevilla();
+
+	public void actualizarCiudadNombre(String ciudad, String nombre);
+
+	public List<Cliente> getClientesPorCiudad(String ciudad);
 }
--- Java/T4-Ej6/src/main/java/com/example/demo/service/ServicioImpl.java
+++ Java/T4-Ej6/src/main/java/com/example/demo/service/ServicioImpl.java
@@ -1,5 +1,72 @@
 package com.example.demo.service;
 
-public class ClienteServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.modelo.Cliente;
+import com.example.demo.modelo.Direccion;
+import com.example.demo.repository.Repositorio;
+
+@Service
+public class ServicioImpl implements Servicio {
+
+	@Autowired
+	private Repositorio repo;
+
+	@Override
+	public List<Cliente> getClientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente getCliente(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertarCliente(Cliente c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Cliente actualizarCliente(Cliente c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminarCliente(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cliente actualizarDireccion(Integer id, Direccion d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void actualizarSevilla() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actualizarCiudadNombre(String ciudad, String nombre) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Cliente> getClientesPorCiudad(String ciudad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
